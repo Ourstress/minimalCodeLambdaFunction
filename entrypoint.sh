46255fbb6e9d1035a6836687e4bee7598e196811
SG <sgng1@e.ntu.edu.sg>
2019-10-30 07:25:23 +0000

    removed archiving code from entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,6 @@ export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
 export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
 export APINAME="$LAMBDA_FUNC_NAME-API"
-export OVERLAY_S3URL="s3://${BUCKET_NAME}/${LAMBDA_FUNC_NAME}/lambda-deploy.tgz"
-
-rm -f lambda-deploy.zip
-aws s3 cp --acl public-read lambda-deploy-overlay.tgz "$OVERLAY_S3URL"
 
 zip -r ../lambda-deploy.zip *
 
